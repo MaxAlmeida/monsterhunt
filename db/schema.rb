151d19a45b4e062d89cae4588a4ceb052677278d
MaxAlmeida <llewxam100@hotmail.com>
2019-11-27 19:17:50 -0300

    Adding sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191127013008) do
+ActiveRecord::Schema.define(version: 20191127204352) do
 
   create_table "collected_coins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "value"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20191127013008) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "password_digest"
   end
 
   add_foreign_key "collected_coins", "users"
