fa7f76951e09de50576b4490efbd4f4dcf0ad386
MaxAlmeida <llewxam100@hotmail.com>
2019-11-28 13:41:04 -0300

    creating gameplay session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_name(params[:name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, flash: { success: "Welcome #{user.name}" }
+      redirect_to gameplay_url, flash: { success: "Welcome #{user.name}" }
     else
       flash.now[:error] = "Name or password is invalid"
       render "new"
@@ -16,5 +16,11 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Logout!"
+  end
+
+  def game_play
+    @user = User.find_by_id(session[:user_id])
+
+    redirect_to login_url, flash: { notice: "Need login to play" } unless @user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'gameplay', to: 'sessions#game_play', as: 'gameplay'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
