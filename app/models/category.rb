a989ef6f2a7d54b7d11dff543b89b1d018ab850b
MaxAlmeida <llewxam100@hotmail.com>
2019-11-29 01:38:09 -0300

    Fixing score process in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,10 @@ class Category < ApplicationRecord
     self.abstract_class = true
     belongs_to :user
     after_create_commit :process_score
-
     def process_score        
-        category_score = self.user.collected_coins.count
+        category_score = self.class.where(user_id: self.user_id).count
         category_name = self.class.name
+        puts category_name
         user_id = self.user_id
         level_score = LevelScore.find_by category: category_name, score: category_score
         Trophy.create(user_id: user_id, level_score_id: level_score.id) if level_score
