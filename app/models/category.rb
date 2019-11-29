2faaee041e0cc621b000d4da40b6e5bf15b5f30c
MaxAlmeida <llewxam100@hotmail.com>
2019-11-28 23:30:23 -0300

    Fixing process score

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,19 +2,12 @@ class Category < ApplicationRecord
     self.abstract_class = true
     belongs_to :user
     after_create_commit :process_score
+
     def process_score        
-        puts "Entrei pontuation"
-        category_score = self.class.count
+        category_score = self.user.collected_coins.count
         category_name = self.class.name
-        puts category_name
         user_id = self.user_id
         level_score = LevelScore.find_by category: category_name, score: category_score
-
-        if level_score
-            Trophy.create(user_id: user_id, level_score_id: level_score.id)
-        else
-            puts "Dont reach pontuation"
-        end
-     end
-
+        Trophy.create(user_id: user_id, level_score_id: level_score.id) if level_score
+    end
 end
--- app/models/killed_monster.rb
+++ app/models/killed_monster.rb
@@ -3,24 +3,17 @@ class KilledMonster < Category
   belongs_to :monster
 
   def process_score
-    puts "Entrei pontuation monster"
-    category_score = KilledMonster.where(monster_id: self.monster.id).count
-    category_name = self.class.name
-    puts category_name
-    user_id = self.user_id
-    level_score = LevelScore.find_by category: category_name, score: category_score
-
-    if level_score
-      puts "entrei if"
       reached_pontuation = false
-      Monster.all.each do |monster|
-        puts "Monster Name: #{monster.name}  score: #{monster.killed_monsters.count}"       
-        reached_pontuation = monster.killed_monsters.count == level_score.score ? true : false
-        puts reached_pontuation
+      category_name = self.class.name
+      monster_score = KilledMonster.where(monster_id: self.monster_id, user_id: self.user_id).count
+      level_score = LevelScore.find_by category: category_name, score: monster_score
+      if level_score
+        Monster.all.each do |monster|
+          killed_monsters_each_monster = KilledMonster.where(monster_id: monster.id, user_id: self.user.id)
+          score_by_monster = killed_monsters_each_monster.count
+          reached_pontuation = score_by_monster == level_score.score ? true : false
+        end
+        Trophy.create(user_id: user_id, level_score_id: level_score.id) if reached_pontuation
       end
-      Trophy.create(user_id: user_id, level_score_id: level_score.id) if reached_pontuation
-    else
-        puts "Dont reach pontuation"
     end
-  end
 end
