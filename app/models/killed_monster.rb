3f192e956ccfe92f762408bf038f58c019058e61
MaxAlmeida <llewxam100@hotmail.com>
2019-11-30 00:40:53 -0300

    Refactoring score process killed monsters

--- app/models/killed_monster.rb
+++ app/models/killed_monster.rb
@@ -6,23 +6,28 @@ class KilledMonster < Category
       puts "Entre killedmonster"
       category_name = self.class.name
       monster_score = KilledMonster.where(monster_id: self.monster_id, user_id: self.user_id).size
-      puts "Monster #{self.monster.name} score: #{monster_score}"
       level_score = LevelScore.find_by category: category_name, score: monster_score
-      reached_score = true if level_score
-      if reached_score
-        reached_others_monsters = Array.new
-        Monster.where.not(id: self.monster_id).each do |monster|
-          killed_monsters_each_monster = KilledMonster.where(monster_id: monster.id, user_id: self.user.id)
-          score_by_monster = killed_monsters_each_monster.size
-          puts "monster name: #{monster.name} score: #{score_by_monster}" 
-          reached_others_monsters << (score_by_monster >= level_score.score ? true : false)
-          puts "Ponntuação : #{reached_others_monsters}"
-        end
-        unless reached_others_monsters.include? false
+      puts "Monster #{self.monster.name} score: #{monster_score}"
+      
+      #Reach some level score?
+      if level_score
+        unless others_reached_score(level_score).include? false
           Trophy.create(user_id: user_id, level_score_id: level_score.id) 
         end
       end
+  end
+
+  def others_reached_score(level_score)
+    check_reached_score = Array.new
+    Monster.where.not(id: self.monster_id).each do |monster|
+      killed_monsters_each_monster = KilledMonster.where(monster_id: monster.id, user_id: self.user.id)
+      score_by_monster = killed_monsters_each_monster.size
+      puts "monster name: #{monster.name} score: #{score_by_monster}" 
+      check_reached_score << (score_by_monster >= level_score.score ? true : false)
+      puts "Ponntuação : #{check_reached_score}"
     end
-    
+    check_reached_score
+  end
+
 end
 
