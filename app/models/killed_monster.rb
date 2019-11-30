571279a281e698ac66c262f4c04a2da48070ea14
MaxAlmeida <llewxam100@hotmail.com>
2019-11-30 00:06:37 -0300

    fixing processing score for KilledMonster

--- app/models/killed_monster.rb
+++ app/models/killed_monster.rb
@@ -3,17 +3,26 @@ class KilledMonster < Category
   belongs_to :monster
 
   def process_score
-      reached_pontuation = false
+      puts "Entre killedmonster"
       category_name = self.class.name
-      monster_score = KilledMonster.where(monster_id: self.monster_id, user_id: self.user_id).count
+      monster_score = KilledMonster.where(monster_id: self.monster_id, user_id: self.user_id).size
+      puts "Monster #{self.monster.name} score: #{monster_score}"
       level_score = LevelScore.find_by category: category_name, score: monster_score
-      if level_score
-        Monster.all.each do |monster|
+      reached_score = true if level_score
+      if reached_score
+        reached_others_monsters = Array.new
+        Monster.where.not(id: self.monster_id).each do |monster|
           killed_monsters_each_monster = KilledMonster.where(monster_id: monster.id, user_id: self.user.id)
-          score_by_monster = killed_monsters_each_monster.count
-          reached_pontuation = score_by_monster == level_score.score ? true : false
+          score_by_monster = killed_monsters_each_monster.size
+          puts "monster name: #{monster.name} score: #{score_by_monster}" 
+          reached_others_monsters << (score_by_monster >= level_score.score ? true : false)
+          puts "Ponntuação : #{reached_others_monsters}"
         end
-        Trophy.create(user_id: user_id, level_score_id: level_score.id) if reached_pontuation
+        unless reached_others_monsters.include? false
+          Trophy.create(user_id: user_id, level_score_id: level_score.id) 
+        end
       end
     end
+    
 end
+
