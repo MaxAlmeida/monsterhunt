cbe4d9658495b9ff5faadabbf143f809f8c02f19
MaxAlmeida <llewxam100@hotmail.com>
2019-11-30 14:43:59 -0300

    Adding scored_categories method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
     has_many :trophies
 
     validates :name, presence: true
+
+    #get categories scored by user
+    def scored_categories
+        trophies = self.trophies
+        trophies.pluck(:level_score_id).map {|id| LevelScore.find_by_id(id).category }.uniq
+    end
 end
