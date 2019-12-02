dc1acce832cdd00094412bfdb5f9f4205f2d5c07
MaxAlmeida <llewxam100@hotmail.com>
2019-12-02 15:50:03 -0300

    Implemeting available level score by hash

--- app/models/level_score.rb
+++ app/models/level_score.rb
@@ -1,3 +1,10 @@
 class LevelScore < ApplicationRecord
     has_many :trophy
+    @@available_level_scores = self.all.to_a.map(&:serializable_hash)
+    
+    def self.available_level_scores(category, score)
+        @@available_level_scores.select do |levelscore| 
+            levelscore['category'] == category && levelscore['score'] == score 
+        end[0]
+    end
 end
