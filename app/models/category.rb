89b5f6ddee5d5e758966682bc46341e03ebc0ded
MaxAlmeida <llewxam100@hotmail.com>
2019-12-02 17:46:18 -0300

    Refactoring category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,16 @@
 class Category < ApplicationRecord
     self.abstract_class = true
     belongs_to :user
+
     after_create_commit :process_score
+
     def process_score        
-        category_score = self.class.where(user_id: self.user_id).count
+        category_score = self.class.where(user_id: self.user_id).size
         category_name = self.class.name
-        puts category_name
         user_id = self.user_id
-        level_score = LevelScore.find_by category: category_name, score: category_score
-        Trophy.create(user_id: user_id, level_score_id: level_score.id) if level_score
+        #level_score = LevelScore.where(category: category_name, score: category_score)[0]
+        level_score = LevelScore.available_level_scores(category_name, category_score)
+        Trophy.create(user_id: user_id, level_score_id: level_score['id']) if level_score
+        #Trophy.create(user_id: user_id, level_score_id: level_score.id) if level_score
     end
 end
