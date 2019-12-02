0d800e766ca56dbbf026a15ec04b9a5876b17abe
MaxAlmeida <llewxam100@hotmail.com>
2019-12-02 17:53:32 -0300

    comment puts log in killed monster

--- app/models/killed_monster.rb
+++ app/models/killed_monster.rb
@@ -3,17 +3,15 @@ class KilledMonster < Category
   belongs_to :monster
 
   def process_score
-      puts "Entre killedmonster"
       category_name = self.class.name
       monster_score = KilledMonster.where(monster_id: self.monster_id, user_id: self.user_id).size
       level_score = LevelScore.find_by category: category_name, score: monster_score
-      puts "Monster #{self.monster.name} score: #{monster_score}"
+      #puts "Monster #{self.monster.name} score: #{monster_score}"
       
       #reach some level score?
       if level_score
         unless others_reached_score(level_score).include? false
           Trophy.create(user_id: user_id, level_score_id: level_score.id) 
-          flash.now[:success] = "Name or password is invalid"
         end
       end
   end
@@ -24,9 +22,9 @@ class KilledMonster < Category
     Monster.where.not(id: self.monster_id).each do |monster|
       killed_monsters_each_monster = KilledMonster.where(monster_id: monster.id, user_id: self.user.id)
       score_by_monster = killed_monsters_each_monster.size
-      puts "monster name: #{monster.name} score: #{score_by_monster}" 
+      #puts "monster name: #{monster.name} score: #{score_by_monster}" 
       check_reached_score << (score_by_monster >= level_score.score ? true : false)
-      puts "Pontuação : #{check_reached_score}"
+     # puts "Pontuação : #{check_reached_score}"
     end
     check_reached_score
   end
